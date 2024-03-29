4548c2bc7dafa5f4d5135de3c598a252d7425953
V-Rang <venu@Venu>
2024-03-29 11:12:40 -0500

    testing CI using ruff

--- test_folder/test_script.py
+++ test_folder/test_script.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pickle
 
-with open('outputs.pickle', 'rb') as f:
+with open("outputs.pickle", "rb") as f:
     data = pickle.load(f)
 
-arr_1 = data['arr_1']
-arr_2 = data['arr_2']
-value = data['val_1']
+arr_1 = data["arr_1"]
+arr_2 = data["arr_2"]
+value = data["val_1"]
 
 print("1")
